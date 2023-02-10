f70798c9652aa3c4fc38f2889c6f45be01b847ab
JastinXyz <jastinlinggar@gmail.com>
2023-02-10 20:18:52 +0700

    head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,11 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" data-theme="dark">
-      <Head />
+      <Head>
+        <html data-theme="dark" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="a software engineering student who loves to code."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
